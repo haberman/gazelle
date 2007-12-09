6b117bc8f29135d28c3f01b393b88b73c5e4241a
Joshua Haberman <joshua@reverberate.org>
2007-12-09 09:34:59 -0800

    add Makefiles for all the new directories. a top-level 'make' should make everything, though it's only currently tested on OS X.

--- utilities/bitcode_dump.c
+++ utilities/bitcode_dump.c
@@ -2,15 +2,47 @@
 #include "bc_read_stream.h"
 
 #include <stdio.h>
+#include <string.h>
 
-int main()
+void check_error(struct bc_read_stream *s)
+{
+    if(bc_rs_get_error(s))
+    {
+        int err = bc_rs_get_error(s);
+        fprintf(stderr, "There were stream errors!\n");
+        if(err & BITCODE_ERR_VALUE_TOO_LARGE)
+            fprintf(stderr, "  Value too large.\n");
+        if(err & BITCODE_ERR_NO_SUCH_VALUE)
+            fprintf(stderr, "  No such value.\n");
+        if(err & BITCODE_ERR_IO)
+            fprintf(stderr, "  IO error.\n");
+        if(err & BITCODE_ERR_CORRUPT_INPUT)
+            fprintf(stderr, "  Corrupt input.\n");
+        if(err & BITCODE_ERR_INTERNAL)
+            fprintf(stderr, "  Internal error.\n");
+    }
+}
+
+void usage()
+{
+    printf("bitcode_dump: dumps all of the records in a bitcode file\n");
+    printf("Usage: bitcode_dump <bitcode file>\n");
+}
+
+int main(int argc, char *argv[0])
 {
     int nesting = 0;
 
-    struct bc_read_stream *s = bc_rs_open_file("/tmp/test.bc");
+    if(argc < 2 || strcmp(argv[1], "--help") == 0)
+    {
+        usage();
+        return 1;
+    }
+
+    struct bc_read_stream *s = bc_rs_open_file(argv[1]);
     if(!s)
     {
-        printf("Failed to open bitcode file!\n");
+        printf("Failed to open bitcode file %s\n", argv[1]);
         return 1;
     }
 
@@ -40,32 +72,16 @@ int main()
         }
         else if(ri.record_type == Eof)
         {
-            printf("Hit EOF.  Bye.\n");
             bc_rs_close_stream(s);
             return 0;
         }
         else if(ri.record_type == Err)
         {
-            printf("Hit an error.  :(\n");
+            fprintf(stderr, "Hit an error.  :(\n");
+            check_error(s);
             return 1;
         }
-
-        if(bc_rs_get_error(s))
-        {
-            int err = bc_rs_get_error(s);
-            printf("There were stream errors!\n");
-            if(err & BITCODE_ERR_VALUE_TOO_LARGE)
-                printf("  Value too large.\n");
-            if(err & BITCODE_ERR_NO_SUCH_VALUE)
-                printf("  No such value.\n");
-            if(err & BITCODE_ERR_IO)
-                printf("  IO error.\n");
-            if(err & BITCODE_ERR_CORRUPT_INPUT)
-                printf("  Corrupt input.\n");
-            if(err & BITCODE_ERR_INTERNAL)
-                printf("  Internal error.\n");
-            return 1;
-        }
+        check_error(s);
     }
 }
 
